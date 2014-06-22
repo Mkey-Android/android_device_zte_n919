fe6501971c1bb839874416f08fb8af1264380dc6
Mkey_NJU <499238317@qq.com>
2014-06-22 19:46:01 +0800

    fix strings

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "ZTE-N909"
+#define BTM_DEF_LOCAL_NAME   "ZTE-N919"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
